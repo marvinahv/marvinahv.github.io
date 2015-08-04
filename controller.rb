72cd763b7a740681ec621f1420f712c8f97299ba
Marvin Alejandro Herrera Vizuett <marvinahv@gmail.com>
2015-08-04 06:10:16 -0500

    Recommendations

--- controller.rb
+++ controller.rb
@@ -6,4 +6,5 @@ before 'index.html.slim' do
   @experience = YAML.load(File.open('data/experience.yml'))
   @skills = YAML.load(File.open('data/skills.yml'))
   @portfolio = YAML.load(File.open('data/portfolio.yml'))
+  @recommendations = YAML.load(File.open('data/recommendations.yml'))
 end
