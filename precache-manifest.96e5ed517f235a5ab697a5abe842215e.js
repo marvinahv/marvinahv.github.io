8ac306570218dc5013459870fd206eddf31568ab
Marvin Alejandro Herrera Vizuett <marvinahv@gmail.com>
2020-04-24 10:56:09 +0000

    Updates

---
+++ precache-manifest.96e5ed517f235a5ab697a5abe842215e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3876791d06f8d69d2381da355f69b90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaee26cd073de0bb9282",
+    "url": "/static/css/main.1f440270.chunk.css"
+  },
+  {
+    "revision": "a5f4326c36c481d576fd",
+    "url": "/static/js/2.cfae9d37.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/static/js/2.cfae9d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaee26cd073de0bb9282",
+    "url": "/static/js/main.f909ced4.chunk.js"
+  },
+  {
+    "revision": "5b1348b2229cf52d6e57",
+    "url": "/static/js/runtime-main.4df5e3f0.js"
+  }
+]);
